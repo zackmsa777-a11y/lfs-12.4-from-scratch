613c57797a5a4c4b218d3a440d464c19cbeaa2db
LFS Builder <builder@lfs-project.local>
2026-07-23 20:28:48 +0000

    Fix glib build: add -D tests=false to skip test suite needing not-yet-installed libgio

--- scripts_chroot/blfs-60-glib.sh
+++ scripts_chroot/blfs-60-glib.sh
@@ -3,6 +3,7 @@ cd /sources
 rm -rf glib-2.84.4
 tar xf glib-2.84.4.tar.xz
 cd glib-2.84.4
+if [ -e /usr/include/glib-2.0 ]; then rm -rf /usr/include/glib-2.0.old && mv -vf /usr/include/glib-2.0{,.old}; fi
 mkdir build && cd build
 meson setup .. \
   --prefix=/usr \
@@ -10,8 +11,9 @@ meson setup .. \
   -D introspection=disabled \
   -D glib_debug=disabled \
   -D man-pages=disabled \
-  -D sysprof=disabled
+  -D sysprof=disabled \
+  -D tests=false
 ninja -j17
 ninja install
 cd /sources
-rm -rf glib-2.84.4
+rm -rf glib-2.84.4 /usr/include/glib-2.0.old
