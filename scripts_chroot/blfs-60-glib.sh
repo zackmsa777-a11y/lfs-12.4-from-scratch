137ea0e40dd0114d608e2681f57bca26785b7d28
LFS Builder <builder@lfs-project.local>
2026-07-23 20:24:25 +0000

    Batch12 script: glib-2.84.4 (minimal, no introspection)

---
+++ scripts_chroot/blfs-60-glib.sh
@@ -0,0 +1,18 @@
+set -e
+cd /sources
+rm -rf glib-2.84.4
+tar xf glib-2.84.4.tar.xz
+cd glib-2.84.4
+if [ -e /usr/include/glib-2.0 ]; then rm -rf /usr/include/glib-2.0.old && mv -vf /usr/include/glib-2.0{,.old}; fi
+mkdir build && cd build
+meson setup .. \
+  --prefix=/usr \
+  --buildtype=release \
+  -D introspection=disabled \
+  -D glib_debug=disabled \
+  -D man-pages=disabled \
+  -D sysprof=disabled
+ninja -j17
+ninja install
+cd /sources
+rm -rf glib-2.84.4 /usr/include/glib-2.0.old
