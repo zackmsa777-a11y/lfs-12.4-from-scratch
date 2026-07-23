9e0a520bc03ba5a3fd0f5a0e4f3ed4371a472089
LFS Builder <builder@lfs-project.local>
2026-07-23 22:50:10 +0000

    Fix VTE build: manually pre-populate fast_float subproject (no git in chroot)

--- scripts_chroot/blfs-92-vte.sh
+++ scripts_chroot/blfs-92-vte.sh
@@ -3,6 +3,13 @@ cd /sources
 rm -rf vte-0.80.3
 tar xf vte-0.80.3.tar.gz
 cd vte-0.80.3
+# fast_float subproject needs git to auto-fetch; pre-populate it manually instead
+rm -rf subprojects/fast_float
+mkdir -p subprojects/fast_float
+tar xf /sources/fast_float-6.1.6.tar.gz -C /tmp
+cp -r /tmp/fast_float-6.1.6/* subprojects/fast_float/
+cp subprojects/packagefiles/fast_float/meson.build subprojects/fast_float/meson.build
+rm -rf /tmp/fast_float-6.1.6
 mkdir build && cd build
 meson setup --prefix=/usr --buildtype=release -D _systemd=false -D gnutls=false -D vapi=false -D gtk4=false -D docs=false -D icu=false ..
 ninja -j17
