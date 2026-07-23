cdf1305ec4559718b8be7b5ba781b9df1fae3f23
LFS Builder <builder@lfs-project.local>
2026-07-23 21:55:58 +0000

    Fix libwnck: disable introspection (g-ir-scanner not built)

--- scripts_chroot/blfs-83-libwnck.sh
+++ scripts_chroot/blfs-83-libwnck.sh
@@ -4,7 +4,7 @@ rm -rf libwnck-43.2
 tar xf libwnck-43.2.tar.xz
 cd libwnck-43.2
 mkdir build && cd build
-meson setup --prefix=/usr --buildtype=release ..
+meson setup --prefix=/usr --buildtype=release -D introspection=disabled ..
 ninja -j17
 ninja install
 cd /sources
