df7a4c5bcc9732a82871fadea298f1348cfa3b3f
LFS Builder <builder@lfs-project.local>
2026-07-23 20:29:43 +0000

    Fix glib build: add -D tests=false (test binary couldn't find just-built libgio at build time)

--- scripts_chroot/blfs-60-glib.sh
+++ scripts_chroot/blfs-60-glib.sh
@@ -3,7 +3,6 @@ cd /sources
 rm -rf glib-2.84.4
 tar xf glib-2.84.4.tar.xz
 cd glib-2.84.4
-if [ -e /usr/include/glib-2.0 ]; then rm -rf /usr/include/glib-2.0.old && mv -vf /usr/include/glib-2.0{,.old}; fi
 mkdir build && cd build
 meson setup .. \
   --prefix=/usr \
@@ -16,4 +15,4 @@ meson setup .. \
 ninja -j17
 ninja install
 cd /sources
-rm -rf glib-2.84.4 /usr/include/glib-2.0.old
+rm -rf glib-2.84.4
