2ec2087d978f21fff5687d617438d84abec44b00
LFS Builder <builder@lfs-project.local>
2026-07-23 20:06:05 +0000

    Fix graphite2 build: add -DCMAKE_POLICY_VERSION_MINIMUM=3.5 for old CMakeLists.txt

--- scripts_chroot/blfs-59-graphite2.sh
+++ scripts_chroot/blfs-59-graphite2.sh
@@ -4,7 +4,7 @@ rm -rf graphite2-1.3.14
 tar xf graphite2-1.3.14.tgz
 cd graphite2-1.3.14
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
 make -j17
 make install
 cd /sources
