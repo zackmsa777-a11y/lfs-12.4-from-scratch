07897b7ea4f8da0829422f4a8a57ffca6e234c56
LFS Builder <builder@lfs-project.local>
2026-07-23 10:53:15 +0000

    fix libxkbfile build: uses meson not autotools

--- scripts_chroot/blfs-30-libxkbfile.sh
+++ scripts_chroot/blfs-30-libxkbfile.sh
@@ -3,8 +3,9 @@ cd /sources
 rm -rf libxkbfile-1.2.0
 tar xf libxkbfile-1.2.0.tar.xz
 cd libxkbfile-1.2.0
-./configure --prefix=/usr --disable-static
-make
-make install
+mkdir -p build
+meson setup build --prefix=/usr --buildtype=release
+ninja -C build
+ninja -C build install
 cd /sources
 rm -rf libxkbfile-1.2.0
