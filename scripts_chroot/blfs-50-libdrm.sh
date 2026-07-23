cb3885d32d63ee7a1bd2b1a90043778734c4a838
LFS Builder <builder@lfs-project.local>
2026-07-23 18:53:24 +0000

    libdrm rebuild: add header install verification

--- scripts_chroot/blfs-50-libdrm.sh
+++ scripts_chroot/blfs-50-libdrm.sh
@@ -5,7 +5,9 @@ tar xf libdrm-2.4.134.tar.xz
 cd libdrm-2.4.134
 mkdir build && cd build
 meson setup --prefix=/usr --buildtype=release -D udev=true -D valgrind=disabled ..
-ninja
+ninja -j17
 ninja install
+test -f /usr/include/xf86drm.h
+test -f /usr/include/xf86drmMode.h
 cd /sources
 rm -rf libdrm-2.4.134
